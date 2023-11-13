8b64f05f27b0e53a596131db68dc5a1fac45eed7
Colin Terry <colin.terry@comset.co.uk>
2023-11-13 15:12:42 +0000

    Removed RestJSONTableResults

--- source/sap.sample.cmsdbdriver.plugin/src/sap/sample/cmsdbdriver/plugin/custom/Plugin.java
+++ source/sap.sample.cmsdbdriver.plugin/src/sap/sample/cmsdbdriver/plugin/custom/Plugin.java
@@ -20,7 +20,6 @@ public class Plugin implements IPlugin {
     public Plugin(final CMSDriverConnection connection) {  
     	pluginBase = new PluginBase(connection);
     	pluginBase.addTable(new TemplateTableResults(pluginBase));
-    	pluginBase.addTable(new RestJSONTableResults(pluginBase));
     	pluginBase.addTable(new ScheduleInfoTableResults(pluginBase));
     }
 
